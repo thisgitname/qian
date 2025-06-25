1612f0453dae54f049f1e9d36ef220024b0240ea
这是gitname <2806348881@qq.com>
2025-06-25 09:37:57 +0800

    commit

--- src/pages/Study/Test.jsx
+++ src/pages/Study/Test.jsx
@@ -3,29 +3,24 @@ import { useNavigate, useLocation } from "react-router-dom";
 import { NavBar, CapsuleTabs, Button } from "antd-mobile";
 import styles from "./Test.module.css";
 
-// 导入图片
-import 古代文学 from '../../../public/img/古代文学概述.png';
-import 当代文学 from '../../../public/img/当代文学概述.png';
-import 文学史 from '../../../public/img/文学史.png';
-
 export default function Test() {
     const navigate = useNavigate();
     const location = useLocation();
     const { course, practiceTime, examTime } = location.state || {};
     const [activeKey, setActiveKey] = useState('a');
     const [list, setList] = useState([
-        { id: 1, title: "古典文学", cate: "practice", course: "文学", img: 古代文学, page: 1 },
-        { id: 2, title: "现代文学", cate: "practice", course: "文学", img: 文学史, page: 2 },
-        { id: 3, title: "当代文学考试", cate: "test", course: "文学", img: 当代文学, page: 1 },
-        { id: 4, title: "近代文学考试", cate: "test", course: "文学", img: 文学史, page: 2 },
-        { id: 5, title: "政治练习1", cate: "practice", course: "政治", img: 文学史, page: 1 },
-        { id: 6, title: "政治练习2", cate: "practice", course: "政治", img: 文学史, page: 2 },
-        { id: 7, title: "政治模拟考试1", cate: "test", course: "政治", img: 文学史, page: 1 },
-        { id: 8, title: "政治模拟考试2", cate: "test", course: "政治", img: 文学史, page: 2 },
-        { id: 9, title: "历史练习1", cate: "practice", course: "历史", img: 文学史, page: 1 },
-        { id: 10, title: "历史练习2", cate: "practice", course: "历史", img: 文学史, page: 2 },
-        { id: 11, title: "历史模拟考试1", cate: "test", course: "历史", img: 文学史, page: 1 },
-        { id: 12, title: "历史模拟考试2", cate: "test", course: "历史", img: 文学史, page: 2 },
+        { id: 1, title: "古典文学", cate: "practice", course: "文学", img: "/img/gudaiwenxue.png", page: 1 },
+        { id: 2, title: "现代文学", cate: "practice", course: "文学", img: "/img/wenxueshi.png", page: 2 },
+        { id: 3, title: "当代文学考试", cate: "test", course: "文学", img: "/img/dangdaiwenxue.png", page: 1 },
+        { id: 4, title: "近代文学考试", cate: "test", course: "文学", img: "/img/wenxueshi.png", page: 2 },
+        { id: 5, title: "政治练习1", cate: "practice", course: "政治", img: "/img/wenxueshi.png", page: 1 },
+        { id: 6, title: "政治练习2", cate: "practice", course: "政治", img: "/img/wenxueshi.png", page: 2 },
+        { id: 7, title: "政治模拟考试1", cate: "test", course: "政治", img: "/img/wenxueshi.png", page: 1 },
+        { id: 8, title: "政治模拟考试2", cate: "test", course: "政治", img: "/img/wenxueshi.png", page: 2 },
+        { id: 9, title: "历史练习1", cate: "practice", course: "历史", img: "/img/wenxueshi.png", page: 1 },
+        { id: 10, title: "历史练习2", cate: "practice", course: "历史", img: "/img/wenxueshi.png", page: 2 },
+        { id: 11, title: "历史模拟考试1", cate: "test", course: "历史", img: "/img/wenxueshi.png", page: 1 },
+        { id: 12, title: "历史模拟考试2", cate: "test", course: "历史", img: "/img/wenxueshi.png", page: 2 },
     ]);
 
     // 处理返回按钮点击
